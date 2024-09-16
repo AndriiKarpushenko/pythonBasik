bb631efdaed894bbcf9fcd751edc3801bc3c149a
Andrii_Karpushenko <avk7777@yahoo.com>
2024-09-16 15:06:03 +0200

    HOme work 2.1

--- Home_work_2_1.py
+++ Home_work_2_1.py
@@ -1,2 +1,14 @@
-user_input = input("Please enter a 4-digit number: ")
-# the_first_number
+# Home work 2.1
+
+four_digit_number = input("Please enter a 4-digit number: ")
+
+four_digit_number=int (four_digit_number)
+first_digit=four_digit_number//1000
+second_digit=(four_digit_number % 1000)//100
+third_digit=(four_digit_number % 100)//10
+fourth_digit=(four_digit_number % 10)
+
+print(first_digit)
+print(second_digit)
+print(third_digit)
+print(fourth_digit)
