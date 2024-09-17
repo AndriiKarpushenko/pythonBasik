35d984908ff1ad4bd57236fc0eb903e985ba0e69
Andrii_Karpushenko <avk7777@yahoo.com>
2024-09-18 00:12:46 +0200

    Hщme work 2.2

--- Home_work_3_1.py
+++ Home_work_3_1.py
@@ -1,1 +1,23 @@
 # Home work 3.1
+
+first_number = input("Please enter the first number: ")
+first_number = float (first_number)
+
+second_number = input("Please enter the second number: ")
+second_number = float (second_number)
+
+action = input("Select an action: (+, -, *, /) ")
+
+if action == '+':
+    calculation_result = first_number + second_number
+elif action == '-':
+    calculation_result = first_number - second_number
+elif action == '*':
+    calculation_result = first_number * second_number
+elif action == '/':
+    if second_number == 0:
+        calculation_result = "Error: Division by zero!"
+    else:
+        calculation_result = first_number / second_number
+
+print("Calculation result:", calculation_result)
