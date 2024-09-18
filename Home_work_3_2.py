73256155738101a5070a849feaa0c9d9f4b635eb
Andrii_Karpushenko <avk7777@yahoo.com>
2024-09-18 19:33:48 +0200

    Hщme work 2.2

--- Home_work_3_2.py
+++ Home_work_3_2.py
@@ -1,29 +1,29 @@
 # Home work 3.2
 
-lst_first = [12, 3, 4, 10]
-if len(lst_first ) > 1:
-    last_element = lst_first [-1]
-    lst_first .insert(0, last_element)
-    del lst_first [-1]
-print(lst_first)
+first_lst = [12, 3, 4, 10]
+if len(first_lst) > 1:
+    last_element = first_lst [-1]
+    first_lst .insert(0, last_element)
+    del first_lst [-1]
+print(first_lst)
 
-lst_second = [12, 3, 4, 10, 8]
-if len(lst_second) > 1:
-    last_element = lst_second[-1]
-    lst_second.insert(0, last_element)
-    lst_second.pop()
-print(lst_second)
+second_lst = [12, 3, 4, 10, 8]
+if len(second_lst) > 1:
+    last_element = second_lst[-1]
+    second_lst.insert(0, last_element)
+    second_lst.pop()
+print(second_lst)
 
-lst_third = [1]
-if len(lst_third) > 1:
-    last_element = lst_third[-1]
-    lst_third.insert(0, last_element)
-    lst_third.pop()
-print(lst_third)
+third_lst = [1]
+if len(third_lst) > 1:
+    last_element = third_lst[-1]
+    third_lst.insert(0, last_element)
+    third_lst.pop()
+print(third_lst)
 
-lst_fourth = [ ]
-if len(lst_fourth ) > 1:
-    last_element = lst_fourth [-1]
-    lst_fourth .insert(0, last_element)
-    del lst_fourth [-1]
-print(lst_fourth)
+fourth_lst = [ ]
+if len(fourth_lst) > 1:
+    last_element = fourth_lst [-1]
+    fourth_lst .insert(0, last_element)
+    del fourth_lst [-1]
+print(fourth_lst)
