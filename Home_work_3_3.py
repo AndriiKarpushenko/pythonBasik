715ca963508e4aec42354f48cbb2c0fec38164c6
Andrii_Karpushenko <avk7777@yahoo.com>
2024-09-19 10:34:12 +0200

    Hщme work 2.2

--- Home_work_3_3.py
+++ Home_work_3_3.py
@@ -11,15 +11,3 @@ else:
     new_list  = [first_new_list, second_new_list]
 
 print(new_list)
-
-original_list = [1, 2, 3, 4, 5]
-
-if len(original_list) == 0:
-    new_list = [[], []]
-else:
-    middle_list  = len(original_list ) // 2
-    first_new_list = original_list[:middle_list + 1]
-    second_new_list = original_list[middle_list + 1:]
-    new_list  = [first_new_list, second_new_list]
-
-print(new_list)
