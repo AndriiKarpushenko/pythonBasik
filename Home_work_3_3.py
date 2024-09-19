61d6a2149739a870137f48eaa9c1476c4f23eac9
Andrii_Karpushenko <avk7777@yahoo.com>
2024-09-19 06:04:50 +0200

    Hщme work 2.2

--- Home_work_3_3.py
+++ Home_work_3_3.py
@@ -6,8 +6,20 @@ if len(original_list) == 0:
     new_list = [[], []]
 else:
     middle_list  = len(original_list ) // 2
-    first_half = original_list[:middle_list + 1]
-    second_half = original_list[middle_list + 1:]
-    new_list  = [first_half, second_half]
+    first_new_list = original_list[:middle_list + 1]
+    second_new_list = original_list[middle_list + 1:]
+    new_list  = [first_new_list, second_new_list]
 
-print(new_list )
+print(new_list)
+
+original_list = [1, 2, 3, 4, 5]
+
+if len(original_list) == 0:
+    new_list = [[], []]
+else:
+    middle_list  = len(original_list ) // 2
+    first_new_list = original_list[:middle_list + 1]
+    second_new_list = original_list[middle_list + 1:]
+    new_list  = [first_new_list, second_new_list]
+
+print(new_list)
