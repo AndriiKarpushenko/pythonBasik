0d627e2fa58b8b209efbc6a13b3c5443df6ad0d5
Andrii_Karpushenko <avk7777@yahoo.com>
2024-09-21 20:48:13 +0200

    Hщme work 2.2

---
+++ Home_work_5_2.py
@@ -0,0 +1,33 @@
+# Home work 5.2
+
+while True:
+
+    first_number = float(input("Please enter the first number: "))
+    second_number = float(input("Please enter the second number: "))
+
+
+    # Запитуємо у користувача дію, яку він хоче виконати
+    action = input("Select an action: (+, -, *, /) ")
+
+    if action == '+':
+        calculation_result = first_number + second_number
+    elif action == '-':
+        calculation_result = first_number - second_number
+    elif action == '*':
+        calculation_result = first_number * second_number
+    elif action == '/':
+        if second_number == 0:
+            calculation_result = "Error: Division by zero!"
+        else:
+            calculation_result = first_number / second_number
+    else:
+        calculation_result = "Invalid action!"
+
+    print("Calculation result:", calculation_result)
+
+
+    continue_calculation = input("Do you want to perform another calculation? (yes/y to continue): ").lower()
+
+    if continue_calculation not in ("yes", "y"):
+        print("Thank you for using the calculator")
+        break
