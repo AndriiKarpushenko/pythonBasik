3cb834dc23a9053e93f562ce3c0e196b28267feb
Andrii_Karpushenko <avk7777@yahoo.com>
2024-09-27 23:47:08 +0200

    Hщme work 2.2

---
+++ Home_work_7_2.py
@@ -0,0 +1,19 @@
+# Home work 7.2
+
+def correct_sentence(text):
+
+    corrected_text = text[0].upper() + text[1:]
+
+    if corrected_text[-1] != '.':
+        corrected_text += '.'
+
+    return corrected_text
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+
+print('ОК')
