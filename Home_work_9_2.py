b6a26ee4cdd25a71c9bf8a2a80caea4cd8eb8862
Andrii_Karpushenko <avk7777@yahoo.com>
2024-10-06 22:44:49 +0200

    Hщme work 2.2

--- Home_work_9_2.py
+++ Home_work_9_2.py
@@ -1,16 +1,21 @@
-# Home work 9.2.
+# Home work 9.2
 
 def difference(*args):
     if len(args) == 0:
         return 0
 
-    maximum_value_of_argument = max(args)
-    minimum_value_of_argument = min(args)
+    maximum_value_of_argument = args[0]
+    minimum_value_of_argument = args[0]
+
+    for argumet in args[1:]:
+        if argumet > maximum_value_of_argument:
+            maximum_value_of_argument = argumet
+        elif argumet < minimum_value_of_argument:
+            minimum_value_of_argument = argumet
 
     result_calculation = round(maximum_value_of_argument - minimum_value_of_argument, 2)
 
     return result_calculation
-
 
 print(difference(1, 2, 3))  # Виведе 2
 print(difference(5, -5))  # Виведе 10
