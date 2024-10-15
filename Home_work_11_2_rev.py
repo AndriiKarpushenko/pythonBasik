345ba02ab4db570267f4e17db8d0e83ea89855cc
Andrii_Karpushenko <avk7777@yahoo.com>
2024-10-15 14:53:53 +0200

    Home work

---
+++ Home_work_11_2_rev.py
@@ -0,0 +1,18 @@
+# Home Work 11.2__rev
+
+def generate_cube_numbers(end):
+    cubes_of_numbers = (number ** 3 for number in range(2, end + 1))
+    for cube in cubes_of_numbers:
+        if cube > end:
+            return
+        yield cube
+
+from inspect import isgenerator
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+
+print("Ok")
