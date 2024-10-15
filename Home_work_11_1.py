1354cb6e2cf46dfb36e28f960a487de6af21ed6e
Andrii_Karpushenko <avk7777@yahoo.com>
2024-10-15 11:14:30 +0200

    Home work

---
+++ Home_work_11_1.py
@@ -0,0 +1,24 @@
+# Home Work 11.1
+
+def prime_generator(end):
+    number = 2
+    while number <= end:
+        prime_number_test = number > 1 and (number == 2 or number % 2 != 0)
+        if prime_number_test:
+            for i in range(3, int(number**0.5) + 1, 2):
+                if number % i == 0:
+                    prime_number_test = False
+                    break
+        if prime_number_test:
+            yield number
+        number += 1
+
+
+from inspect import isgenerator
+gen = prime_generator(1)
+assert isgenerator(gen) == True, 'Test0'  # Проверка, что функция является генератором
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'  # Проверка простых чисел до 10
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'  # Проверка простых чисел до 15
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'  # Проверка простых чисел до 29
+
+print('Ok')
